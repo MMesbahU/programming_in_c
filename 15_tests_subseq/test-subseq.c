19dedd45f7f4e523179ea5209009e81930df741a
Coursera Learner <nobody@nowhere.nul>
2019-03-30 10:16:47 +0000

    15_tests

---
+++ 15_tests_subseq/test-subseq.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+size_t maxSeq(int *array, size_t n);
+
+// Compare different Array
+void ans(int *array, size_t n, size_t answer)
+{
+  if (maxSeq(array, n) != answer )
+  {
+    exit(EXIT_FAILURE);
+  }
+}
+// Main Function
+int main()
+{
+  int arr1[] = {-1, 0, 1};
+  ans(arr1, 3, 3);
+
+  int arr2[] = {3, 3, 4, 4, 5};
+  ans(arr2, 5, 2);
+ 
+  int arr3[] = {1, 2, 3, 3, 4, 5};
+  ans(arr3, 6, 3);
+
+  int arr4[] ={0};
+  ans(arr4, 0, 0);
+  
+  int arr5[] = {100, 99, 98};
+  ans(arr5, 3, 1); 
+ 
+  return(EXIT_SUCCESS);
+}
+
+
